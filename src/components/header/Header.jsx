011259987cb6a09a222d2a2edc922fa879162b3b
Aniket <aniketdhokane2071@gmail.com>
2022-12-26 18:59:38 +0530

    Start

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {AppBar,Toolbar} from '@mui/material'
+
+
+const Header = () => {
+  return (
+    <div>
+        <AppBar>
+            <Toolbar>
+
+            </Toolbar>
+        </AppBar>
+    </div>
+  )
+}
+
+export default Header
